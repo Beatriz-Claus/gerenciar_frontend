19b5cf29f4f2f28314faf81a9f7d118838b7eb9b
biaclaus21@outlook.com <biaclaus21@outlook.com>
2021-01-08 08:42:51 -0300

    Frontend - Iniciado (.then ATUALIZADO!!!)

--- gerir_frontend/src/pages/login/index.js
+++ gerir_frontend/src/pages/login/index.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import {Container, Form, Button} from 'react-bootstrap';
-import logo from '../../logo.svg'
-import './index.css'
+import  logo from '../../logo.svg';
+import './index.css';
 
 const Login = () => {
 
+    //public string Email { get; set; };
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
-    const logar = (event) =>{
-        console.log(`${email} - ${senha}`);
-        event.preventDefault();
+    //Chama o evento de logar na Api
+    const logar = (event) => {
+        event.preventDefault();        
 
+        //representa o objeto que contém
+        //os dados do usuário para login
         const objLogin = {
             "email" : email,
             "senha" : senha
@@ -19,31 +22,36 @@ const Login = () => {
 
         console.log(objLogin);
 
-        fetch('http://localhost:65242/api/usuario/login', {
+        fetch('http://localhost:65242/api/usuario/login',{
             method : 'POST',
             body : JSON.stringify(objLogin),
             headers : {
                 'content-type' : 'application/json'
             }
-        })  
-        .then(response =>{
-            console.log(response)
         })
+        .then(response => {
+            //Verifica se a resposta da Api esta ok
+            if(response.ok){
+                return response.json();
+            }
 
-        // .then(response =>{
-        //     if(response.ok){
-        //         //faça isto
-        //     }
+            //Caso tenha retornado algum erro da api informa 
+            //o usário
+            alert("Dados inválidos");
+        })
+        .then(data => {
+            console.log(data);
 
-        //         alert("Dados Invalidos");
-        // })
-        
+            localStorage.setItem('token-gerir', data.token);
+
+            //Navegar para as tarefas
+        })
     }
 
-    return(
-
-        <Container className='form-height'>
-                <Form className='form-signin' onSubmit={event =>logar(event)}>
+    return (
+        <div>
+            <Container className='form-height'>
+                <Form className='form-signin' onSubmit={event => logar(event)}>
                     <div className='text-center'>
                      <img src={logo} alt='NYOUS' style={{ width : '64px'}} />
                     </div>
@@ -52,12 +60,12 @@ const Login = () => {
                     <hr/>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email </Form.Label>
-                        <Form.Control type="email" value={email} onChange= {event => setEmail(event.target.value)} placeholder="Informe o email" required />
+                        <Form.Control type="email" value={email} onChange={event => setEmail(event.target.value)} placeholder="Informe o email" required />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" value={senha} onChange= {event => setSenha(event.target.value)} placeholder="Senha"  required/>
+                        <Form.Control type="password" value={senha} onChange={event => setSenha(event.target.value)} placeholder="Senha"  required/>
                     </Form.Group>
                     <Button variant="primary" type="submit" >
                         Enviar
@@ -66,7 +74,8 @@ const Login = () => {
                     <a href='/cadastrar' style={{ marginTop :'30px'}}>Não tenho conta!</a>
                 </Form>
             </Container>
-
-        )
+        </div>
+    );
 }
+
 export default Login;
